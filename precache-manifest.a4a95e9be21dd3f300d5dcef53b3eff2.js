ce99c96fb05f92f5a8167aa81827dd5330ea10a9
Naveen Sekar <c.naveensekar@gmail.com>
2019-12-12 17:44:48 +0530

    Updates

---
+++ precache-manifest.a4a95e9be21dd3f300d5dcef53b3eff2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bea3ddc50864edc7586fcdf87a2bae0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d600eb1411d3a499e7a",
+    "url": "/static/css/main.c073ca3e.chunk.css"
+  },
+  {
+    "revision": "d00f9f47f641e2157794",
+    "url": "/static/js/2.3669446b.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/static/js/2.3669446b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2d600eb1411d3a499e7a",
+    "url": "/static/js/main.aa731f02.chunk.js"
+  },
+  {
+    "revision": "d8baa8ab250e22b08caa",
+    "url": "/static/js/runtime-main.e5ea4907.js"
+  }
+]);
